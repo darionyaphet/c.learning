1e9f85d9335386c4cf7044caa12ddf637e162102
darion.yaphet <darion.yaphet@gmail.com>
2018-12-23 00:33:38 +0800

    template

---
+++ template_example.cpp
@@ -0,0 +1,28 @@
+//
+// Created by darion.yaphet on 2018/12/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+inline T const &Max(T const &a, T const &b) {
+    return a < b ? b : a;
+}
+
+int main() {
+    int i = 39;
+    int j = 20;
+    cout << "Max(i, j): " << Max(i, j) << endl;
+
+    double f1 = 13.5;
+    double f2 = 20.7;
+    cout << "Max(f1, f2): " << Max(f1, f2) << endl;
+
+    string s1 = "Hello";
+    string s2 = "World";
+    cout << "Max(s1, s2): " << Max(s1, s2) << endl;
+
+    return 0;
+}
